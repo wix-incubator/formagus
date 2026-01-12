18d80e40e85086f0f5c314761526913211f9bfc4
iaroslavs <iaroslavs@wix.com>
2026-01-12 18:17:59 +0200

    fix(all): fix repo links

--- packages/docs/docusaurus.config.mjs
+++ packages/docs/docusaurus.config.mjs
@@ -20,7 +20,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'iaroslavshvets', // Usually your GitHub org/user name.
+  organizationName: 'wix-incubator', // Usually your GitHub org/user name.
   projectName: 'formagus', // Usually your repo name.
 
   onBrokenLinks: 'throw',
